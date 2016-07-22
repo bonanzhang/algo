882ce6cfe7d1ad96626660e979be75a72edc3a7b
Bonan Zhang <bonan.zhang@gmail.com>
2016-07-21 19:44:32 -0700

    this is embarassing, all previous commits were incorrect

--- src/qsort.cc
+++ src/qsort.cc
@@ -22,11 +22,28 @@ long partition(int *a, long lo, long hi) {
 
 void quicksort_recurse(int *a, long lo, long hi) {
     long p;
-    if ((hi-lo) > 1) {
-        p = partition(a, lo, hi);
-        quicksort_recurse(a, lo, p-1);
-        quicksort_recurse(a, p+1, hi);
+    long i0;
+    long i1;
+    long i2;
+    long i_med;
+    if ((hi-lo+1) <= 1) {
+        return;
     }
+    i0 = lo;
+    i1 = (hi-lo)/2+lo;
+    i2 = hi;
+    if (a[i1] <= a[i0] && a[i0] <= a[i2]) i_med = i0;
+    if (a[i2] <= a[i0] && a[i0] <= a[i1]) i_med = i0;
+    if (a[i0] <= a[i1] && a[i1] <= a[i2]) i_med = i1;
+    if (a[i2] <= a[i1] && a[i1] <= a[i0]) i_med = i1;
+    if (a[i0] <= a[i2] && a[i2] <= a[i1]) i_med = i2;
+    if (a[i1] <= a[i2] && a[i2] <= a[i0]) i_med = i2;
+    long temp = a[lo];
+    a[lo] = a[i_med];
+    a[i_med] = temp;
+    p = partition(a, lo, hi);
+    quicksort_recurse(a, lo, p-1);
+    quicksort_recurse(a, p+1, hi);
 }
 
 void quicksort(int *a, long n) {
--- test/qsort_test.cc
+++ test/qsort_test.cc
@@ -1,5 +1,21 @@
 #include "qsort.h"
 #include <gtest/gtest.h>
+TEST(QuickSortTest, HandlesQuizCase) {
+    int arr[] = {3,9,8,4,6,10,2,5,7,1};
+    long i;
+    long sorted;
+    long sz = sizeof(arr)/sizeof(arr[0]);
+    quicksort(arr, sz);
+    sorted = 1;
+    for (i = 1; i < sz; i++){
+        if (arr[i-1] > arr[i]) {
+            sorted = 0;
+            break;
+        }
+    }
+    EXPECT_EQ(sorted, 1);
+}
+
 TEST(QuickSortTest, HandlesSortedCase) {
     int arr[] = {1,2,3,4,5,6,7,8};
     long i;
