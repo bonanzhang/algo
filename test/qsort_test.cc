553375919787bf8798b089020f802d0492faccb5
Bonan Zhang <bonan.zhang@gmail.com>
2016-07-21 17:17:21 -0700

    added more test cases

--- test/qsort_test.cc
+++ test/qsort_test.cc
@@ -1,7 +1,39 @@
 #include "qsort.h"
 #include <gtest/gtest.h>
+TEST(QuickSortTest, HandlesSortedCase) {
+    int arr[] = {1,2,3,4,5,6,7,8};
+    long i;
+    long sorted;
+    long sz = sizeof(arr)/sizeof(arr[0]);
+    quicksort(arr, sz);
+    sorted = 1;
+    for (i = 1; i < sz; i++){
+        if (arr[i-1] > arr[i]) {
+            sorted = 0;
+            break;
+        }
+    }
+    EXPECT_EQ(sorted, 1);
+}
+
+TEST(QuickSortTest, HandlesEqualElementsCase) {
+    int arr[] = {3,1,2,5,1,3,4,2};
+    long i;
+    long sorted;
+    long sz = sizeof(arr)/sizeof(arr[0]);
+    quicksort(arr, sz);
+    sorted = 1;
+    for (i = 1; i < sz; i++){
+        if (arr[i-1] > arr[i]) {
+            sorted = 0;
+            break;
+        }
+    }
+    EXPECT_EQ(sorted, 1);
+}
+
 TEST(QuickSortTest, HandlesCommonCase) {
-    int arr[] = {1,3,5,2,4,6};
+    int arr[] = {7,8,1,3,5,2,4,6};
     long i;
     long sorted;
     long sz = sizeof(arr)/sizeof(arr[0]);
