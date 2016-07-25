405e669c5a9559ecfc5661d006066810549b5f26
Bonan Zhang <bonan.zhang@gmail.com>
2016-07-24 21:50:39 -0700

    more test cases for local minimum

--- test/local_minimum_test.cc
+++ test/local_minimum_test.cc
@@ -1,17 +1,32 @@
 #include "local_minimum.h"
 #include <gtest/gtest.h>
-TEST(LocalMinimumTest, HandlesCommonCase) {
-    int arr[] = {16, 2, 3,13,
-                  5,11,10, 8,
-                  9, 7, 6,12,
-                  4,14,15, 1};
-    long r;
-    long c;
-    find_local_minimum(arr, 4, &r, &c);
+TEST(LocalMinimumTest, HandlesEdgeCase) {
+    long r,c;
+    int arr[] = {5,1,3,
+                 2,4,6,
+                 0,9,7};
+    find_local_minimum(arr, 3, &r, &c);
     EXPECT_EQ(r, 0);
     EXPECT_EQ(c, 1);
 }
-
+TEST(LocalMinimumTest, HandlesCornerCase) {
+    long r,c;
+    int arr[] = {0,1,2,
+                 3,4,5,
+                 6,7,8};
+    find_local_minimum(arr, 3, &r, &c);
+    EXPECT_EQ(r, 0);
+    EXPECT_EQ(c, 0);
+}
+TEST(LocalMinimumTest, HandlesCommonCase) {
+    long r,c;
+    int arr[] = {5,2,6,
+                 1,0,3,
+                 7,4,8};
+    find_local_minimum(arr, 3, &r, &c);
+    EXPECT_EQ(r, 1);
+    EXPECT_EQ(c, 1);
+}
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
