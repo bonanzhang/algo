05d3224566e5e6e62147f9756aef2a2c89f45601
Bonan Zhang <bonan.zhang@gmail.com>
2016-08-04 09:50:47 -0700

    started using git lfs

--- src/SCC_finder.cc
+++ src/SCC_finder.cc
@@ -1,18 +1,21 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include "graph.h"
 int main() {
-    int src;
-    int dst;
-    long counter;
+    std::string src;
+    std::string dst;
+    Graph g;
     std::string line;
     std::stringstream ss;
-    counter = 0;
     while (std::getline(std::cin, line)) {
         ss << line;
         ss >> src >> dst;
-        counter++;
+        ss.clear();
+        Vertex u(src);
+        Vertex v(dst);
+        g.addEdge(u,v);
     }
-    std::cout << "edge count: " << counter << std::endl;
+    std::cout << g << std::endl;;
     return 0;
 }
