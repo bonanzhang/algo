118c1f02e98eff85d72e25468fe04c0e2869c8bf
Bonan Zhang <bonan.zhang@gmail.com>
2016-08-13 17:40:30 -0700

    dijkstra's implemented, untested

--- include/weighted_graph.h
+++ include/weighted_graph.h
@@ -4,13 +4,20 @@
 #include <map>
 #include <vector>
 #include <iostream>
-typedef std::map<Vertex, std::vector<std::pair<Vertex, int> > > adj_list;
+#include <algorithm>
+typedef std::vector<std::pair<Vertex, int> > neighbor_list;
+typedef std::map<Vertex, neighbor_list> adj_list;
 class WeightedGraph {
     public:
          WeightedGraph();
          void addEdge(Vertex u, Vertex v, int dist);
+         std::map<Vertex, int> computeShortestPaths(Vertex s);
          friend std::ostream& operator<<(std::ostream &os, const WeightedGraph &wg);
     private:
         adj_list graph_;
+        std::vector<Vertex> queue_;
+        void insertIntoQueue(Vertex v);
+        Vertex findMinimumFromQueue(std::map<Vertex, int> dist);
+        void deleteFromQueue(Vertex v);
 };
 #endif // WEIGHTED_GRAPH_H
--- src/weighted_graph.cc
+++ src/weighted_graph.cc
@@ -2,11 +2,68 @@
 WeightedGraph::WeightedGraph() {
     graph_.clear();
 }
-void WeightedGraph::addEdge(Vertex u, Vertex v, int dist) {
+void WeightedGraph::addEdge(Vertex u, Vertex v, int l) {
     if (graph_.find(u) != graph_.end()) {
-        graph_.insert(std::pair<Vertex, std::vector<std::pair<Vertex, int> > >(u, vector<Vertex, int>());
+        graph_.insert(std::pair<Vertex, neighbor_list >(u, neighbor_list()));
     }
-    graph_.find(u)->second.push_back(std::pair<Vertex, int>(v, dist));
+    graph_.find(u)->second.push_back(std::pair<Vertex, int>(v, l));
+}
+void WeightedGraph::insertIntoQueue(Vertex v) {
+    queue_.push_back(v);
+}
+Vertex WeightedGraph::findMinimumFromQueue(std::map<Vertex, int> dist) {
+    Vertex res;
+    int cur_min = 2000000;
+    for (std::vector<Vertex>::const_iterator it = queue_.begin(); it != queue_.end(); it++) {
+        if (dist[*it] < cur_min) {
+            cur_min = dist[*it];
+            res = *it;
+        }
+    }
+    return res;
+}
+void WeightedGraph::deleteFromQueue(Vertex v) {
+    queue_.erase(std::find(queue_.begin(), queue_.end(), v));
+}
+std::map<Vertex, int> WeightedGraph::computeShortestPaths(Vertex s) {
+    //vertex set Q
+    queue_.clear();
+    //distances dist
+    std::map<Vertex, int> dist;
+    //for each v in G
+    for (adj_list::const_iterator it = graph_.begin(); it != graph_.end(); ++it) {
+        Vertex v = it->first;
+        //dist[v] = INFINITE
+        dist[v] = 1000000;
+        //add v to Q
+        insertIntoQueue(v);
+    }
+    //dist[s] = 0;
+    dist[s] = 0;
+    //while Q is not empty
+    while (queue_.size() > 0) {
+        //u = min(Q)
+        Vertex u = findMinimumFromQueue(dist);
+        //remove u from Q
+        deleteFromQueue(u);
+        //for each neighbor v of u
+        neighbor_list nl;
+        if (graph_.find(u) != graph_.end()) {
+            nl = graph_.find(u)->second;
+        }
+        for (neighbor_list::const_iterator it = nl.begin(); it != nl.end(); ++it) {
+            //alt = dist[v] + l(u,v)
+            Vertex v = it->first;
+            int l_uv = it->second;
+            int alt = dist[v] + l_uv;
+            //if alt < dist[v]
+            if (alt < dist[v]) {
+                //dist[v] = alt
+                dist[v] = alt;
+            }
+        }
+    }
+    return dist;
 }
 std::ostream& operator<<(std::ostream &os, const WeightedGraph &wg) {
     return os;
